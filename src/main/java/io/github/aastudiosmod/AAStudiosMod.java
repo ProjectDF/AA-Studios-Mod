8dfcca553e0936653da73489da2116d5763beb9b
BuildTools <unconfigured@null.spigotmc.org>
2021-08-13 13:54:50 -0500

    literally

---
+++ src/main/java/io/github/aastudiosmod/AAStudiosMod.java
@@ -0,0 +1,11 @@
+package io.github.aastudiosmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AAStudiosMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("awesome");
+    }
+}
